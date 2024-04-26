4a4a90075738b326f0685a1d5d93759cd63836a2
kqhuynh <anhkieu303252@gmail.com>
2024-04-26 10:43:50 +0700

    feat: add geoLocation & category data

---
+++ src/utils/category.ts
@@ -0,0 +1,84 @@
+export const category = [
+  {
+    cat_id: 1000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/1000.png',
+    cat_name: 'Bất động sản',
+    position: 1,
+  },
+  {
+    cat_id: 13000,
+    cat_icon:
+      'https://static.chotot.com/storage/chapy-pro/newcats/v8/13000.png',
+    cat_name: 'Việc làm',
+    position: 2,
+  },
+  {
+    cat_id: 2000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/2000.png',
+    cat_name: 'Xe cộ',
+    position: 3,
+  },
+  {
+    cat_id: 12000,
+    cat_icon:
+      'https://static.chotot.com/storage/chapy-pro/newcats/v8/12000.png',
+    cat_name: 'Thú cưng',
+    position: 4,
+  },
+  {
+    cat_id: 5000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/5000.png',
+    cat_name: 'Đồ điện tử',
+    position: 5,
+  },
+  {
+    cat_id: 7000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/7000.png',
+    cat_name: 'Đồ ăn, thực phẩm và các loại khác',
+    position: 6,
+  },
+  {
+    cat_id: 14000,
+    cat_icon:
+      'https://static.chotot.com/storage/chapy-pro/newcats/v8/14000.png',
+    cat_name: 'Đồ gia dụng, nội thất, cây cảnh',
+    position: 7,
+  },
+  {
+    cat_id: 9000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/9000.png',
+    cat_name: 'Tủ lạnh, máy lạnh, máy giặt',
+    position: 8,
+  },
+  {
+    cat_id: 4000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/4000.png',
+    cat_name: 'Giải trí, Thể thao, Sở thích',
+    position: 9,
+  },
+  {
+    cat_id: 3000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/3000.png',
+    cat_name: 'Thời trang, Đồ dùng cá nhân',
+    position: 10,
+  },
+  {
+    cat_id: 11000,
+    cat_icon:
+      'https://static.chotot.com/storage/chapy-pro/newcats/v8/11000.png',
+    cat_name: 'Mẹ và bé',
+    position: 11,
+  },
+  {
+    cat_id: 8000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/8000.png',
+    cat_name: 'Đồ dùng văn phòng, công nông nghiệp',
+    position: 12,
+  },
+  {
+    cat_id: 6000,
+    cat_icon: 'https://static.chotot.com/storage/chapy-pro/newcats/v8/6000.png',
+    cat_name: 'Dịch vụ, Du lịch',
+    position: 13,
+  },
+];
