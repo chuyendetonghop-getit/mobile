c6772ae3408be2b09fc96aa7f27e07bf432a36c6
B460M <anhkieu303252@gmail.com>
2024-05-31 22:03:48 +0700

    chore: Update ESLint configuration to fix error and improve code formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
+  // fix: ESLint show error in module.export above
+  requireConfigFile: false,
   root: true,
   extends: '@react-native',
+  // below configuration is to make sure prettier and eslint play nice together on Mac and Windows
+  // https://prettier.io/docs/en/options.html#end-of-line
+  // https://stackoverflow.com/questions/53516594/why-do-i-keep-getting-eslint-delete-cr-prettier-prettier
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-unused-vars': 'off',
+    // 'react-native/no-inline-styles': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 };
